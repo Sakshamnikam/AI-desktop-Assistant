fff8ee814850a63f057aad72a959cd0230c82a5c
Saksham Nikam <sakshamnikam2007@gmial.com>
2025-12-13 22:25:05 +0530

    Configuration

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+import speech_recognition as sr
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# Shared recognizer (calibrated once)
+recognizer = sr.Recognizer()
+
+# Conversation memory list
+conversation_history = []
